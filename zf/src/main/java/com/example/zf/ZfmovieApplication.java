8d68b81cad2c072f93914908d0fdc7adff50fe09
Turtl3e <matklucz97@gmail.com>
2020-05-16 15:11:11 +0200

    Initialize java project

---
+++ zf/src/main/java/com/example/zf/ZfmovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.zf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZfmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZfmovieApplication.class, args);
+	}
+
+}
---
+++ zf/src/test/java/com/example/zf/ZfmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZfmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
