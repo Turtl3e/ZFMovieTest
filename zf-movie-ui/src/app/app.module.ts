7922a336f2086b2a5c47fc1ea1e4473e9d265382
Turtl3e <matklucz97@gmail.com>
2020-05-19 18:32:51 +0200

    Create nav and welcome component

--- zf-movie-ui/src/app/app.module.ts
+++ zf-movie-ui/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    WelcomeComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
